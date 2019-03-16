123ed466f98e2649fdacf11d6bcbe3f7f05fbc0e
carolyn <c.e.day@live.com>
2019-03-16 19:41:31 -0400

    fixing sql

--- conference.sql
+++ conference.sql
@@ -1,10 +1,12 @@
 -- create the database for the conference 
 CREATE DATABASE conferencedb; 
+USE conferencedb;
 
 -- type of attendee to categorize attendees 
 CREATE TABLE AttendeeTypes (
     AttendeeType varchar(20) NOT NULL,
-    AttendanceCost decimal(13,2) NOT NULL
+    AttendanceCost decimal(13,2) NOT NULL,
+    PRIMARY KEY (AttendeeType)
 );
 
 -- attendee has an attendee type 
@@ -14,7 +16,9 @@ CREATE TABLE Attendees (
     LastName varchar(20) NOT NULL,
     AttendeeType varchar(20), -- an attendee has a type 
     PRIMARY KEY (ID),
-    FOREIGN KEY (AttendeeType) REFERENCES AttendeeTypes(AttendeeType)
+    CONSTRAINT FOREIGN KEY (AttendeeType) 
+    REFERENCES AttendeeTypes (AttendeeType) 
+    ON DELETE SET NULL 
 );
 
 -- students may decided to stay in a room 
@@ -33,8 +37,8 @@ CREATE TABLE StudentAttendees (
     LastName varchar(20) NOT NULL, -- PK
     RoomNumber int, -- FK
     PRIMARY KEY (FirstName, LastName), 
-    FOREIGN KEY (RoomNumber) REFERENCES RoomingArrangements(RoomNumber),
-    FOREIGN KEY (ID) REFERENCES Attendees(ID)
+    FOREIGN KEY (RoomNumber) REFERENCES RoomingArrangements(RoomNumber) ON DELETE SET NULL,
+    FOREIGN KEY (ID) REFERENCES Attendees(ID) ON DELETE CASCADE
 );
 
 -- Professionals are attendees of conference 
@@ -61,7 +65,8 @@ CREATE TABLE SpeakerAttendees (
 CREATE TABLE SponsorLevels (
     SponsorLevel varchar(20) NOT NULL, 
     FinancialSupport decimal(13,2), 
-    EmailsAllowed int
+    EmailsAllowed int,
+    PRIMARY KEY (SponsorLevel)
 );
 
 -- sponsors are allowed to have job postings 
@@ -84,15 +89,15 @@ CREATE TABLE Sponsors (
     FOREIGN KEY (JobPosting) REFERENCES JobPostings(JobTitle),
     FOREIGN KEY (SponsorLevel) REFERENCES SponsorLevels(SponsorLevel)
 );
---I don't think we need this bc we have the Sponsors already attached as an attendee type
-/* CREATE TABLE SponsorAttendees (
-    ID int NOT NULL, -- PK
-    FirstName varchar(20) NOT NULL,
-    LastName varchar(20) NOT NULL,
-    Company varchar(20) NOT NULL,
-    PRIMARY KEY (FirstName, LastName),
-    FOREIGN KEY (Company) REFERENCES Sponsors(CompanyName)
-); */ 
+-- I don't think we need this bc we have the Sponsors already attached as an attendee type
+-- CREATE TABLE SponsorAttendees (
+--     ID int NOT NULL, -- PK
+--     FirstName varchar(20) NOT NULL,
+--     LastName varchar(20) NOT NULL,
+--     Company varchar(20) NOT NULL,
+--     PRIMARY KEY (FirstName, LastName),
+--     FOREIGN KEY (Company) REFERENCES Sponsors(CompanyName)
+-- ); 
 
 -- speaker speaks at a session 
 CREATE TABLE ScheduleInformation (
@@ -101,15 +106,21 @@ CREATE TABLE ScheduleInformation (
     StartTime time(4),
     EndTime time(4),
     RoomLocation varchar(20), 
-    PRIMARY KEY (SessionName),
-    FOREIGN KEY (SpeakerID) REFERENCES SpeakerAttendees(ID),
-    FOREIGN KEY (SessionName) REFERENCES SpeakerAttendees(SessionSpeakingAt)
+    PRIMARY KEY (StartTime, RoomLocation),
+    FOREIGN KEY (SpeakerID) REFERENCES Attendees (ID) ON DELETE CASCADE
 );
+
+-- CREATE TABLE SpeakerAttendees (
+--     SpeakerID int NOT NULL,
+--     SessionName varchar(20) NOT NULL,
+--     PRIMARY KEY (SpeakerID),
+--     FOREIGN KEY (SpeakerID) REFERENCES Attendee (ID) ON DELETE CASCADE
+-- );
 
 CREATE TABLE OrganizingCommittee (
     SubCommitteeName varchar(20),
     Chair varchar(20),
-    PRIMARY KEY (Chair)
+    PRIMARY KEY (SubCommitteeName)
 );
 
 CREATE TABLE CommitteeMember (
@@ -117,48 +128,6 @@ CREATE TABLE CommitteeMember (
     LastName varchar(20) NOT NULL,
     SubCommitteeName varchar(20), -- multiple entries 
     NumberOfCommittees int, -- derived from subcommittee name 
-    PRIMARY KEY (FirstName, LastName)
-    -- FOREIGN KEY (SubCommitteeName) REFERENCES OrganizingCommittee(SubCommitteeName)
+    PRIMARY KEY (FirstName, LastName),
+    FOREIGN KEY (SubCommitteeName) REFERENCES OrganizingCommittee(SubCommitteeName)
 );
-
-
-delete from Attendees;
-delete from SponsorLevels;
-delete from StudentAttendees;
-delete from SpeakerAttendees;
-delete from ProfessionalAttendees;
-delete from Sponsors;
-delete from RoomingArrangements;
-delete from AttendeeTypes;
-delete from JobPostings;
-delete from ScheduleInformation;
-delete from OrganizationCommittee;
-delete from CommitteeMember;
-insert into SponsorLevels values ('Gold', '5000.0', '2' );
-insert into SponsorLevels values ('Silver', '3000.0', '1');
-insert into SponsorLevels values ('Platinum', '10000.0', '3');
-insert into SponsorLevels values ('Bronze', '1000.0', '0');
-insert into Attendees values ('20', 'Cache', 'Angus', 'StudentAttedees');
-insert into Attendees values ('21', 'Carolyn', 'Day', 'ProfessionalAttendees');
-insert into Attendees values ('22', 'Ted', 'Munn', 'SpeakerAttendees');
-insert into Attendees values ('23', 'Shane', 'Madej', 'Sponsor');
-insert into Attendees values('24', 'Teej', 'Camera', 'SpeakerAttendees');
-insert into RoomingArrangements values ('Cache, Cache2, Cache3', '2', '3', '4');
-insert into StudentAttendees values ('20', 'Cache', 'Angus', '2');
-insert into ProfessionalAttendees values ('21', 'Carolyn', 'Day', 'Computers');
-insert into SpeakerAttendees values ('22', 'Ted', 'Munn', 'QGIC');
-insert into SpeakerAttendees values ('24', 'Teej', 'Camera', 'Ghosts');
-insert into AttendeeTypes values ('StudentAttendees', '50.0');
-insert into AttendeeTypes values ('ProfessionalAttendees', '100.0');
-insert into AttendeeTypes values ('SpeakerAttendees', '0.0');
-insert into AttendeeTypes values ('SponsorAttendees', '0.0');
-insert into Sponsors values ('BuzzFeed Unsolved', 'Gold', 'Video Producer', '2');
-insert into JobPostings values ('Buzzfeed Unsolved', 'Video Producer', '20000', 'Los Angeles', 'California');
-insert into ScheduleInformation values ('QGIC', '22', '10:00:00', '11:00:00', '9');
-insert into ScheduleInformation values ('Ghosts', '24', '11:00:00', '12:00:00', '13');
-insert into OrganizingCommittee values ('Setup','Tom Brady');
-insert into OrganizingCommittee values ('Takedown', 'Ryan Bergara');
-insert into CommitteeMember values ('Tom', 'Brady', 'Setup', '1');
-insert into CommitteeMember values ('Ryan', 'Bergara', 'Takedown, Setup', '2');
-
-
