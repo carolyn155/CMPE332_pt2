14e94a0ce3d1075668f1150859c88a505d12bfc9
carolyn <c.e.day@live.com>
2019-03-20 18:31:41 -0400

    carolyn finished her queries

--- seventhphp.php
+++ seventhphp.php
@@ -10,17 +10,55 @@
 
 
 <?php
-$committee = $_POST["subcommitteename"];
-echo "<h3>You have selected the $committee sub-committee.</h3>";
-echo "<p>Here is a list of all the names of the committee members:</p>";
+echo "<h3>You have asked to see information on the Conference Attendees.</h3>";
+echo "<p>Here lists all of the conference attendees as 3 lists: students, professionals, sponsors.</p>";
 echo "<br>";
+echo "<h4>Students</h4>";
+echo "<p>Here is a list of all the names of the student attendees:</p>";
 echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
 
 #connect to the database
 $pdo = new PDO('mysql:host=localhost;dbname=conferencedb', "root", "");
-$sql = "select FirstName, LastName from CommitteeMember where SubCommitteeName = ?";
+$sql = "select FirstName, LastName from Attendees where AttendeeType = 'Student'";
 $stmt = $pdo->prepare($sql);   #create the query
-$stmt->execute([$committee]);   #bind the parameters
+$stmt->execute();   #bind the parameters
+
+#stmt contains the result of the program execution
+#use fetch to get results row by row.
+while ($row = $stmt->fetch()) {
+	echo "<tr><td>".$row["FirstName"]."</td><td>".$row["LastName"]."</td></tr>";
+}
+echo "</table>";
+echo "<br>";
+
+
+echo "<h4>Professionals</h4>";
+echo "<p>Here is a list of all the names of the professional attendees:</p>";
+echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
+
+#connect to the database
+$pdo = new PDO('mysql:host=localhost;dbname=conferencedb', "root", "");
+$sql = "select FirstName, LastName from Attendees where AttendeeType = 'Professional'";
+$stmt = $pdo->prepare($sql);   #create the query
+$stmt->execute();   #bind the parameters
+
+#stmt contains the result of the program execution
+#use fetch to get results row by row.
+while ($row = $stmt->fetch()) {
+	echo "<tr><td>".$row["FirstName"]."</td><td>".$row["LastName"]."</td></tr>";
+}
+echo "</table>";
+echo "<br>";
+
+echo "<h4>Sponsors</h4>";
+echo "<p>Here is a list of all the names of the sponsor attendees:</p>";
+echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
+
+#connect to the database
+$pdo = new PDO('mysql:host=localhost;dbname=conferencedb', "root", "");
+$sql = "select FirstName, LastName from Attendees where AttendeeType = 'Sponsor'";
+$stmt = $pdo->prepare($sql);   #create the query
+$stmt->execute();   #bind the parameters
 
 #stmt contains the result of the program execution
 #use fetch to get results row by row.
