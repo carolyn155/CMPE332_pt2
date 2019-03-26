0bc079216906542dc447900cf0bcebaf6b2ec49d
Cache Angus <15sa54@caslab.queensu.ca>
2019-03-26 16:36:11 -0400

    the delete works

--- eleventhphp.php
+++ eleventhphp.php
@@ -3,7 +3,6 @@
 <head>
 <link href="style.css" type="text/css" rel="stylesheet" >
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<link rel="stylesheet" href="https://use.typekit.net/yom8ypy.css">
 </head>
 <body>
 <h2>Delete Sponsors</h2>
@@ -13,7 +12,7 @@
 <?php
 $sponsor = $_POST["CompanyName"];
 echo "<h3>You have selected the $sponsor to be deleted.</h3>";
-echo "<p>Here is the updated list of companies:</p>";
+echo "<p>Here is the updated list of companies and the attendee list:</p>";
 echo "<br>";
 echo "<table><tr><th>Companies</th></tr>";
 
@@ -35,8 +34,22 @@ while ($row = $stmt->fetch()) {
 	echo "<tr><td>".$row["CompanyName"]."</td></tr>";
 }
 
+echo"</table>";
+echo "<br/>";
+echo "<table><tr><th>ID</th><th>Company Name</th><tr>";
+
+$sql2 = "select ID, Company from SponsorAttendees ";
+$stmt2 = $pdo->prepare($sql2);
+$stmt2->execute([]);
+
+while ($row = $stmt2->fetch()){
+	echo "<tr><td>".$row["ID"]."</td><td>".$row["Company"]."</td></tr>";
+}
+
 ?>
+
 </table>
+
 </body>
 <br/>
 <br/>
